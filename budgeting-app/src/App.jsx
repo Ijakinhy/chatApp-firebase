79069ec26b025d494e913356ff7c8bb57bca8250
ninijosue <ninijosue123@gmail.com>
2024-03-26 23:31:28 +0200

    starting budget app

---
+++ budgeting-app/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="App">budgetting app</div>;
+}
+
+export default App;
--- youtube-app/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const request = axios.create({
-  baseURL: "https://www.googleapis.com/youtube/v3",
-
-  params: {
-    key: "AIzaSyB2a7r7QSQfx02WAGjS3xUjIG_cPFuds1w",
-  },
-});
