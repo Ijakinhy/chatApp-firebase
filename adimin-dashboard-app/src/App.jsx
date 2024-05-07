53b24fd6836ba8ab496edd70a0aa7529fb6fbc0d
ninijosue <ninijosue123@gmail.com>
2024-05-07 14:07:54 +0200

    save all

--- adimin-dashboard-app/src/App.jsx
+++ adimin-dashboard-app/src/App.jsx
@@ -14,7 +14,7 @@ import Form from "./scenes/form";
 // import Geography from './scenes/geography'
 // import FAQ from './scenes/faq'
 // import Line from './scenes/line'
-// import Calendar from "./scenes/calendar";
+import Calendar from "./scenes/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -33,6 +33,7 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
               {/* <Route path="/bar" element={<Bar/>}/> */}
               {/* <Route path="/pie" element={<Pie/>}/> */}
               {/* <Route path="/geography" element={<Geography/>}/> */}
--- adimin-dashboard-app/src/scenes/calendar/index.jsx
+++
@@ -1,8 +0,0 @@
-import { Box, useTheme } from "@mui/material";
-
-const Calendar = () => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-};
-
-export default Calendar;
