89750e885778258b32040318ea80d49bf599a05b
ninijosue <ninijosue123@gmail.com>
2024-06-12 12:58:13 +0200

    firebase settings

---
+++ ChartApp/src/lib/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "ijakinhy-chart-app.firebaseapp.com",
+  projectId: "ijakinhy-chart-app",
+  storageBucket: "ijakinhy-chart-app.appspot.com",
+  messagingSenderId: "562838929700",
+  appId: "1:562838929700:web:ee4296a7faf78b4a4f73b1",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const db = getFirestore();
+export const storage = getStorage();
